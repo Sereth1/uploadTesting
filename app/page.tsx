b4562015346be914c6d8dc8d5e3728ba9b6bbf55
Sereth1 <n1ghtw0olf@outlook.com>
2024-05-13 16:10:46 +0800

    TROLING IS AN ART

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+  const [isActive, setIsActive] = useState(false);
+  console.log(isActive);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      konte gamiesai
+      <button
+        onClick={() => setIsActive(!isActive)}
+        className="rounded-full bg-blue-500 text-white px-4 py-2"
+      >
+        KANE CLICK
+      </button>
+
+      {isActive && (
+        <>
+          <h1>GAMIESE</h1>
+          <Image src="/nikos.jpg" width={500} height={500} alt="nikolas" />
+        </>
+      )}
     </main>
   );
 }
