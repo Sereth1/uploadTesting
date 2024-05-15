e892f185c9d74a75fbf93138eb15fbb2d4db686e
Sereth1 <n1ghtw0olf@outlook.com>
2024-05-15 16:38:20 +0800

    yyyy

--- app/components/Computer.tsx
+++ app/components/Computer.tsx
@@ -1,17 +1,24 @@
 // src/components/Computer.tsx
 import React, { useState } from "react";
 import DraggableButton from "./Dragging";
+import DragWindows from "./DragWindows";
 
 interface DraggableButtonProps {
   initial: string;
   setInitial: React.Dispatch<React.SetStateAction<string>>;
 }
+
+interface DragWindowsProps {
+  setInitial: React.Dispatch<React.SetStateAction<string>>;
+}
+
 const Computer: React.FC = () => {
   const [initial, setInitial] = useState<string>("start");
 
   return (
     <div className="bg-custom-teal w-screen h-screen fixed top-0 left-0">
       <DraggableButton initial={initial} setInitial={setInitial} />
+      <DragWindows setInitial={setInitial} />
     </div>
   );
 };
